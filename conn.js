fdd22c7dba61272066e6182cb27298cea306a8a3
Lucas Romero <lucas.romerovivas@gmail.com>
2021-03-15 12:45:15 -0300

    conexao bd online

--- conn.js
+++ conn.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql')
+// const conn = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     password : '',
+//     database : 'ecorenda'
+// })
+
 const conn = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'ecorenda'
+    host : 'bcb4zitmaou2stkgdpv6-mysql.services.clever-cloud.com',
+    user : 'utqonpx5q16xfxeg',
+    password : 'Pf2cvnak5D4W8rQj7aR0',
+    database : 'bcb4zitmaou2stkgdpv6'
 })
+
 
 module.exports = conn
